3f9a63abe452ffc768ca8a10b9b8c2092c742d83
lucas PREAUX <lucas.preau@gmail.com>
2023-06-12 12:57:06 +0200

    feat(TP02): :sparkles: JDBC - Connexion avec fichier properties

--- src/main/java/fr/digi/TestConnexionJdbc.java
+++ src/main/java/fr/digi/TestConnexionJdbc.java
@@ -3,14 +3,15 @@ package fr.digi;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
-    public static final String DB_URL = "jdbc:mariadb://localhost:3306/compta";
-    public static final String DB_LOGIN = "root";
-    public static final String DB_PASSWORD = "";
-
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_LOGIN, DB_PASSWORD)) {
+        ResourceBundle databaseConf = ResourceBundle.getBundle("database");
+        String dbUrl = databaseConf.getString("database.url");
+        String dbUser = databaseConf.getString("database.user");
+        String dbPassword = databaseConf.getString("database.password");
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
             System.out.println(connection);
         } catch (SQLException e) {
             throw new RuntimeException(e);

0363f588d426498073c16f0fb70d787eb2da4396
lucas PREAUX <lucas.preau@gmail.com>
2023-06-13 10:57:21 +0200

    feat(TP04): :sparkles: JDBC - Mise en place de DAO
    Correction de la suppression grace à l'enregistrement de l'ID dans l'entité à l'insert

--- src/main/java/fr/digi/jdbc/dao/FournisseurDao.java
+++ src/main/java/fr/digi/jdbc/dao/FournisseurDao.java
@@ -2,11 +2,15 @@ package fr.digi.jdbc.dao;
 
 import fr.digi.jdbc.entites.Fournisseur;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface FournisseurDao {
-    List<Fournisseur> extraire();
-    void insert(Fournisseur fournisseur);
-    int update(String ancienNom, String nouveauNom);
-    boolean delete(Fournisseur fournisseur);
+    List<Fournisseur> extraire() throws SQLException;
+
+    Fournisseur getById(int id) throws SQLException;
+
+    void insert(Fournisseur fournisseur) throws SQLException;
+    int update(String ancienNom, String nouveauNom) throws SQLException;
+    boolean delete(Fournisseur fournisseur) throws SQLException;
 }
--- src/main/java/fr/digi/jdbc/dao/FournisseurDaoJdbc.java
+++ src/main/java/fr/digi/jdbc/dao/FournisseurDaoJdbc.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FournisseurDaoJdbc implements FournisseurDao {
-
     private final Connection connection;
 
     public FournisseurDaoJdbc(Connection connection) {
         this.connection = connection;
     }
+
     @Override
-    public List<Fournisseur> extraire() {
-        List<Fournisseur> fournisseurs = new ArrayList<>();
+    public List<Fournisseur> extraire() throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM FOURNISSEUR")) {
             try (ResultSet resultSet = statement.executeQuery()) {
+                List<Fournisseur> fournisseurs = new ArrayList<>();
                 while (resultSet.next()) {
                     Fournisseur fournisseur = new Fournisseur(
                             resultSet.getInt("ID"),
@@ -25,46 +25,65 @@ public class FournisseurDaoJdbc implements FournisseurDao {
                     );
                     fournisseurs.add(fournisseur);
                 }
+                return fournisseurs;
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
-        return fournisseurs;
     }
 
     @Override
-    public void insert(Fournisseur fournisseur) {
-        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO FOURNISSEUR(NOM) VALUES (?)")) {
+    public Fournisseur getById(int id) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM FOURNISSEUR WHERE ID=?")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return new Fournisseur(
+                            resultSet.getInt("ID"),
+                            resultSet.getString("NOM")
+                    );
+                } else {
+                    throw new RuntimeException("Fournisseur not found");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void insert(Fournisseur fournisseur) throws SQLException {
+        String columnName = "insert_id";
+        String[] columnNames = new String[] {columnName};
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO FOURNISSEUR(NOM) VALUES (?)", columnNames)) {
             statement.setString(1, fournisseur.getNom());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            int executedUpdate = statement.executeUpdate();
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (executedUpdate > 1) {
+                throw new RuntimeException("Too much element where inserted : " + generatedKeys);
+            } else if (executedUpdate == 1) {
+                if(generatedKeys.next()) {
+                    int id = generatedKeys.getInt(columnName);
+                    fournisseur.setID(id);
+                } else {
+                    throw new RuntimeException("No generatedKey found");
+                }
+            } else {
+                throw new RuntimeException("No row inserted");
+            }
         }
     }
 
     @Override
-    public int update(String ancienNom, String nouveauNom) {
-        int result;
+    public int update(String ancienNom, String nouveauNom) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement("UPDATE FOURNISSEUR SET NOM = ? WHERE NOM = ?")) {
             statement.setString(1, nouveauNom);
             statement.setString(2, ancienNom);
-            result = statement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return statement.executeUpdate();
         }
-        return result;
     }
 
     @Override
-    public boolean delete(Fournisseur fournisseur) {
-        boolean result;
+    public boolean delete(Fournisseur fournisseur) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement("DELETE FROM FOURNISSEUR WHERE ID = ?")) {
             statement.setInt(1, fournisseur.getID());
-            result = statement.executeUpdate() > 0;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return statement.executeUpdate() > 0;
         }
-        return result;
     }
 }
